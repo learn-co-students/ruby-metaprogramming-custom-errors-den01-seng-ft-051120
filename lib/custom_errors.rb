a3bd492427b0c75831be4b66a8b337f5bf60192d
matthewmjm <matthewmjm@yahoo.com>
2020-05-31 21:13:28 -0600

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,21 +1,90 @@
 class Person
   attr_accessor :partner, :name
-
+ 
   def initialize(name)
     @name = name
   end
-
+ 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+          puts error.message
+      end
+    else
+      person.partner = self
+    end
   end
-
+ 
+  class PartnerError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
+  end
 end
-
+ 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
 
+#=>
+#you must give the get_married method an argument of an instance of the person class!
+#Beyonce
 
 
 
+
+
+#-----------------------------------------------------------------------------------------
+# class Person
+#   attr_accessor :partner, :name
+ 
+#   def initialize(name)
+#     @name = name
+#   end
+ 
+#   def get_married(person)
+#     self.partner = person
+#     if person.class != Person
+#       raise PartnerError
+#     else
+#       person.partner = self
+#     end
+#   end
+ 
+#   class PartnerError < StandardError
+#   end
+# end
+ 
+# beyonce = Person.new("Beyonce")
+# beyonce.get_married("Jay-Z")
+# puts beyonce.name
+
+# custom_errors.rb:36:in `get_married': Person::PartnerError (Person::PartnerError)
+
+
+
+
+
+#-----------------------------------------------------------------------------------------
+# class Person
+#   attr_accessor :partner, :name
+
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   def get_married(person)
+#     self.partner = person
+#     person.partner = self
+#   end
+
+# end
+
+# beyonce = Person.new("Beyonce")
+# beyonce.get_married("Jay-Z")
+# puts beyonce.name
+
+#custom_errors.rb:10:in `get_married': undefined method `partner=' for "Jay-Z":String (NoMethodError)
